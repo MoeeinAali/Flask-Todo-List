220b63ae07a2cbd5af6795e8f5cd398a00b868d6
Moeein Aali <moeeeinaali@gmail.com>
2024-09-12 15:36:35 +0330

    feat: Add ENUM to models.py

--- models.py
+++ models.py
@@ -1,21 +1,24 @@
 from flask_sqlalchemy import SQLAlchemy
+from enum import Enum
 
 db = SQLAlchemy()
+
+
+class TaskStatus(Enum):
+    ONGOING = 'ongoing'
+    DONE = 'done'
 
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
-    status = db.Column(db.String(20), nullable=False, default='ongoing')
+    status = db.Column(db.Enum(TaskStatus), nullable=False, default=TaskStatus.ONGOING)
 
     def toggle_status(self):
-        if self.status == "ongoing":
-            self.status = "done"
+        if self.status == TaskStatus.ONGOING:
+            self.status = TaskStatus.DONE
         else:
-            self.status = "ongoing"
-
-    def __init__(self, name):
-        self.name = name
+            self.status = TaskStatus.ONGOING
 
     def __repr__(self):
         return f'<Task {self.name}>'
